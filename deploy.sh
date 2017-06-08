efdaee682a45ede179c2ff60bf18919e1088e95d
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-06-08 19:52:09 +0530

    Username should be accpeted as command line argument

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-scp -r website/* tanay@158.69.192.84:~/website
+if [ "$1" == "" ]; then
+    echo "Please enter your username for the server"
+else
+    scp -r website/* $1@158.69.192.84:~/website
+fi
+
