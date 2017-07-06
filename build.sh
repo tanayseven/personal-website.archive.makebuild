729ced704913e102433d397a238af117776e625e
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-07-06 19:51:03 +0530

    Renamed the blog

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ export BLOG_PATH="blog/"
 export BLOG_LINKS_LIST="blog-links-list.txt"
 mkdir -p "$BASE_PATH$BLOG_PATH"
 for filename in posts/*.md; do
-    ./build-blog.sh "$filename" &
+    ./blog-build.sh "$filename" &
 done
 wait
