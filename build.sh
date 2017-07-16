3e3aa911449b3854723a82dfbe370d113886f6bf
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-07-16 16:26:23 +0530

    Add building of the index.html file

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-export MD=mdtrans
-export BASE_PATH="www/"
-export BLOG_PATH="blog/"
-export BLOG_LINKS_HEADINGS="blog-links-headings.txt"
-export BLOG_LINKS_DESCRIPTIONS="blog-links-descriptions.txt"
+export PAGES_PATH='src/pages'
+export SCRIPT_PATH='src/'
+export OUTPUT_PATH='www/'
+export BLOG_PATH='blog/'
 mkdir -p "$BASE_PATH$BLOG_PATH"
+INDEX='index.html'
+python src/build_index.py >"$OUTPUT_PATH$INDEX"
 for filename in posts/*.md; do
     ./blog-build.sh "$filename" &
 done
---
+++ setup_environment.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source "/usr/local/bin/virtualenvwrapper.sh"
+export WORKON_HOME=~/virtualenvs
+mkvirtualenv --python=/usr/bin/python3 personal_website
+workon personal_website
+pip install -r src/requirements.txt
