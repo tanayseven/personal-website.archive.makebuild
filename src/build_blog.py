ddd984e5cf3d07994fb6bcf99bebd89d723192fa
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-07-18 20:13:12 +0530

    Add lock while writing to files, save description to a file for each blog post

--- src/build_blog.py
+++ src/build_blog.py
@@ -1,3 +1,4 @@
+import fcntl
 from jinja2 import Environment, FileSystemLoader
 import sys
 
@@ -9,8 +10,16 @@ html_file_name = '/'.join(sys.argv[1].split('/')[-2:])
 
 
 def write_tags(tags):
-    with open('tags.txt', 'a') as f:
+    with open('tags.csv', 'a') as f:
+        fcntl.fcntl(f.fileno(), fcntl.LOCK_EX)
         f.write(html_file_name + ',' + ','.join(tags) + '\n')
+    return ''
+
+
+def write_description(desc):
+    with open('desc.csv', 'a') as f:
+        fcntl.fcntl(f.fileno(), fcntl.LOCK_EX)
+        f.write(html_file_name + ',' + desc + '\n')
     return ''
 
 
@@ -21,7 +30,7 @@ template = Environment(loader=FileSystemLoader('src/pages/')).get_template(html_
 
 print(template.render(
     css_file_path=css_file_path,
-    page_title='Home - Tanay PrabhuDesai',
     nav_button='home',
     write_tags=write_tags,
+    write_description=write_description,
 ))
