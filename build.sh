664a246e6793e26a62a6e1748bb4c93b997c5d2f
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-07-21 09:57:14 +0530

    Fix for rendering images, unpublish one blog post

--- build.sh
+++ build.sh
@@ -7,18 +7,21 @@ export OUTPUT_PATH='www/'
 BLOG='blog/'
 RES='res/'
 FONTS='fonts/'
+IMAGES='images/'
 export BLOG_PATH="$OUTPUT_PATH$BLOG"
 export RES_PATH="$OUTPUT_PATH$RES"
 export FONTS_PATH="$OUTPUT_PATH$FONTS"
 export RESUME_PATH=$OUTPUT_PATH"resume/"
 export ABOUT_PATH=$OUTPUT_PATH"about/"
+export IMAGE_PATH="$OUTPUT_PATH$RES$IMAGES"
 
 # Create the output directories
-mkdir -p "$BLOG_PATH" "$FONTS_PATH" "$RES_PATH" "$RESUME_PATH" "$ABOUT_PATH"
+mkdir -p "$BLOG_PATH" "$FONTS_PATH" "$RES_PATH" "$RESUME_PATH" "$ABOUT_PATH" "$IMAGE_PATH"
 
 # Build all the static files into one single file
 python ./src/combine_static.py ./src/static/ css "$OUTPUT_PATH" "$RES"
 cp ./src/static/fonts/* "$FONTS_PATH"
+cp -r ./src/static/images/* "$IMAGE_PATH"
 cp ./src/.htaccess "$OUTPUT_PATH"
 
 # Build the home page which is the index.html page
