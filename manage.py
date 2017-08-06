0e19a16e9b5ef2fcaa30300ccce68fcfcbe1fc71
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-08-06 23:57:40 +0530

    Add a clean command

--- manage.py
+++ manage.py
@@ -8,6 +8,9 @@ manager = Manager()
 def build(threads=1):
     print("Starting a build with %d threads ..." % threads)
 
+@manager.command
+def clean():
+    pass
 
 if __name__ == '__main__':
     manager.main()
