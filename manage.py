03380a1042443465d6f1d74afb5fd120dbc3379b
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-08-07 00:04:13 +0530

    Add parallelizing code to build

--- manage.py
+++ manage.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 from manager import Manager
+from multiprocessing import Pool
 
 manager = Manager()
 
 
+def func(period):
+    from time import sleep
+    sleep(period)
+
+
 @manager.command
 def build(threads=1):
+    pool = Pool(threads)
     print("Starting a build with %d threads ..." % threads)
+    pool.map(func, [1, 1, 1, 1, 1])
+
 
 @manager.command
 def clean():
     pass
+
 
 if __name__ == '__main__':
     manager.main()
