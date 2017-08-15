d8ffd5b371ef391dda43f40564bb928b353cc7f9
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-08-15 18:17:39 +0530

    Fix hte deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 if [ "$1" == "" ]; then
     echo "Please enter your username for the server"
 else
-    rsync -a www $1@158.69.192.84:~/website
+    rsync -avz www/ $1@158.69.192.84:~/website/
 fi
 
