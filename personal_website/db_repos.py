fbca77835ca2f94bd0d53d66b7258641eaab0402
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-08-26 21:23:32 +0530

    Add tests for db_repo

--- personal_website/db_repos.py
+++ personal_website/db_repos.py
@@ -1,19 +1,55 @@
 import os
 import sqlite3
+from functools import wraps
 
 from personal_website.constants import DATABASE_NAME
 
+conn = None
 
-def create_initial_database():
-    conn = sqlite3.connect(DATABASE_NAME)
+
+def database_call(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        global conn
+        conn = sqlite3.connect(DATABASE_NAME)
+        ret_val = func(*args, **kwargs)
+        conn.commit()
+        conn.close()
+        return ret_val
+
+    return decorated_function
+
+
+@database_call
+def create_table_minified():
     conn.execute(
         '\n'
         'CREATE TABLE IF NOT EXISTS \n'
         'minified (id INTEGER, name TEXT, content TEXT, type TEXT)'
     )
-    conn.execute("INSERT INTO  minified (id, name, content, type) VALUES "
-                 "(0, 'something', 'alrekflksahfeaowefuoa;ieuf', 'css')")
-    conn.close()
+
+
+@database_call
+def insert_dummy_into_minified():
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO  minified VALUES "
+                 "(0, '', '', '')")
+
+
+def create_initial_database():
+    create_table_minified()
+    insert_dummy_into_minified()
+
 
 def wipe_database():
     os.remove(DATABASE_NAME)
+
+
+class MinifiedRepo:
+    @staticmethod
+    @database_call
+    def get_max_id():
+        cursor = conn.cursor()
+        return cursor.execute(
+            'SELECT MAX(id) FROM minified;'
+        ).fetchone()[0] + 1
--- tests/test_db_repos.py
+++ tests/test_db_repos.py
@@ -1,8 +1,14 @@
 import pytest
 
+from personal_website.db_repos import create_initial_database, MinifiedRepo
 
-class TestMinification:
 
-    @pytest.fixture
-    def initialize(self):
-        pass
+class TestDatabaseRepos:
+    @pytest.yield_fixture(autouse=True)
+    def before_and_after(self):
+        create_initial_database()  # before
+        yield
+        # wipe_database()  # after
+
+    def test_initial_default_max_for_minified_table_is_1(self):
+        assert MinifiedRepo.get_max_id() == 1
