3e2bbb64509d1be53dfabf52b760479cf2d79580
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-09-18 09:44:25 +0530

    Refactored the database classes and files

--- personal_website/db_repos.py
+++ personal_website/db_repos.py
@@ -5,53 +5,13 @@ from functools import wraps
 from constants import DATABASE_NAME
 from personal_website.models import db, BlogPostModel
 
-conn = None
-
-
-def database_call(func):
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        global conn
-        conn = sqlite3.connect(DATABASE_NAME)
-        ret_val = func(*args, **kwargs)
-        conn.commit()
-        conn.close()
-        return ret_val
-
-    return decorated_function
-
-
-@database_call
-def create_table_minified():
-    conn.execute(
-        '\n'
-        'CREATE TABLE IF NOT EXISTS \n'
-        'minified (id INTEGER, name TEXT, content TEXT, type TEXT)'
-    )
-
-
-@database_call
-def insert_dummy_into_minified():
-    conn.execute("INSERT INTO  minified VALUES "
-                 "(0, '', '', '')")
-
 
 def create_initial_database():
-    create_table_minified()
-    insert_dummy_into_minified()
+    pass
 
 
 def wipe_database():
-    os.remove(DATABASE_NAME)
-
-
-class MinifiedRepo:
-    @staticmethod
-    @database_call
-    def get_max_id():
-        return conn.execute(
-            'SELECT MAX(id) FROM minified;'
-        ).fetchone()[0] + 1
+    pass
 
 
 class BlogPostRepo:
--- tests/test_db_repos.py
+++ tests/test_db_repos.py
@@ -1,6 +1,6 @@
 import pytest
 
-from personal_website.db_repos import create_initial_database, MinifiedRepo, wipe_database, BlogPostRepo
+from personal_website.db_repos import create_initial_database, wipe_database, BlogPostRepo
 from personal_website.models import BlogPostModel, db
 
 
@@ -15,16 +15,18 @@ class TestDatabaseRepos:
     def blog_post_repo(self) -> BlogPostRepo:
         return BlogPostRepo()
 
-    def test_initial_default_max_for_minified_table_is_1(self):
-        assert MinifiedRepo.get_max_id() == 1
-
     def test_two_inserted_blog_posts_should_be_retrieved_in_correct_order(self, blog_post_repo):
-        blog_post_2016 = BlogPostModel(src_url='/src/place1', dest_url='/dest/place1', blog_title='', blog_description='', date='2016-30-04')
-        blog_post_2017 = BlogPostModel(src_url='/src/place2', dest_url='/dest/place2', blog_title='', blog_description='', date='2017-30-04')
+        blog_post_2016 = BlogPostModel(
+            src_url='/src/place1', dest_url='/dest/place1', blog_title='',
+            blog_description='', date='2016-30-04'
+        )
+        blog_post_2017 = BlogPostModel(
+            src_url='/src/place2', dest_url='/dest/place2', blog_title='',
+            blog_description='', date='2017-30-04'
+        )
         db.add(blog_post_2016)
         db.add(blog_post_2017)
         db.commit()
         posts = blog_post_repo.get_all_posts_sorted()
         assert posts[0].date == '2016-30-04'
         assert posts[1].date == '2017-30-04'
-
