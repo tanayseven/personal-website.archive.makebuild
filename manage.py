faf88987513ef73d3574c52536dbb752337e6e86
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-09-21 21:53:51 +0530

    Extract endpoint generators

--- manage.py
+++ manage.py
@@ -1,48 +1,15 @@
 #!/usr/bin/env python3
 import os
-from glob import glob
 
-from flask import url_for
 from flask_frozen import Freezer
 from manager import Manager
 
-from constants import BLOG_PATH, IMAGE_PATH
-from personal_website.blog_path import split_path, trim_left_path
+from personal_website.endpoint_generator import freeze_endpoints
 from personal_website.flask_app import app
 
 manager = Manager()
 freezer = Freezer(app)
-
-
-@freezer.register_generator
-def fonts():
-    path = os.path.join(app.root_path, 'static/fonts') + '/'
-    fonts_ = next(os.walk(path))[2]
-    for font in fonts_:
-        yield {'path': font}
-
-
-@freezer.register_generator
-def blog_post():
-    files = next(os.walk(BLOG_PATH))[2]
-    blog_posts = [file for file in files if file.endswith('.html')]
-    for post in blog_posts:
-        path = split_path('', '', post)[0]
-        yield {
-            'year': path[0],
-            'month': path[1],
-            'day': path[2],
-            'name': path[3][:-5],
-        }
-
-
-@freezer.register_generator
-def images():
-    image_dir_ = glob(IMAGE_PATH + '*')
-    for image_dir in image_dir_:
-        files = next(os.walk(image_dir))[2]
-        for image_file in files:
-            yield {'path': trim_left_path(image_dir + '/' + image_file, IMAGE_PATH)}
+freeze_endpoints(freezer)
 
 
 @manager.command
