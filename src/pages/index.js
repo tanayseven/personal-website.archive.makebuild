065e77f5b0dea3810a34634a086419527a421ce2
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-12-24 23:02:28 +0530

    Added react files for gatsby website

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default () => {
+    return
+    ( 
+        <div>
+          <div id="social-media-links">
+              <a class="darkFont" target="_blank" href="https://www.linkedin.com/in/tanay-prabhudesai-1029b073/"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              <a class="darkFont" target="_blank" href="https://plus.google.com/+TanayPrabhuDesai"><i class="fa fa-google" aria-hidden="true"></i></a>
+              <a class="darkFont" target="_blank" href="https://twitter.com/tanayseven"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a class="darkFont" target="_blank" href="https://github.com/tanayseven"><i class="fa fa-github" aria-hidden="true"></i></a>
+          </div>
+          <p>Hi, I'm Tanay PrabhuDesai.</p>
+          <p>I'm a software engineer based in Pune, India.</p>
+          <p>This website is generated using <a href="https://github.com/tanayseven/personal_website/">Frozen Flask</a></p>
+        </div>
+    )
+}
