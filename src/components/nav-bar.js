ab073849817ea0f03fe8e90fc111fef73356a72f
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-12-28 22:53:06 +0530

    Working nav bar with color after copy pasting code

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,20 +1,81 @@
 import React from "react";
 import Tabs, { Tab } from "material-ui/Tabs";
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Typography from 'material-ui/Typography';
 
-export default class NavBar extends React.Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <Tabs>
-                        <Tab disabled label="Tanay PrabhuDesai" />
-                        <Tab href="/" label="Home" />
-                        <Tab href="/blog/" label="Blog" />
-                        <Tab href="/resume/" label="Résumé" />
-                        <Tab href="/about/" label="About Me" />
-                    </Tabs>
-                </nav>
-            </div>
-        )
-    }
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+        {props.children}
+        </Typography>
+    );
 }
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        marginTop: theme.spacing.unit * 3,
+        backgroundColor: theme.palette.background.paper,
+    },
+});
+
+class NavBar extends React.Component {
+    state = {
+      value: 0,
+    };
+  
+    handleChange = (event, value) => {
+      this.setState({ value });
+    };
+  
+    render() {
+      const { classes } = this.props;
+      const { value } = this.state;
+  
+      return (
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab disabled label="Tanay PrabhuDesai" />
+              <Tab label="Home" href="/" />
+              <Tab label="Blog" href="/blog/" />
+              <Tab label="Résumé" href="/resume/" />
+              <Tab label="About Me" href="/about/" />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <TabContainer>Item One</TabContainer>}
+          {value === 1 && <TabContainer>Item Two</TabContainer>}
+          {value === 2 && <TabContainer>Item Three</TabContainer>}
+        </div>
+      );
+    }
+  }
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+  export default withStyles(styles)(NavBar);
+
+// class NavBar extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <nav>
+//                     <Tabs>
+//                         <Tab disabled label="Tanay PrabhuDesai" />
+//                         <Tab href="/" label="Home" />
+//                         <Tab href="/blog/" label="Blog" />
+//                         <Tab href="/resume/" label="Résumé" />
+//                         <Tab href="/about/" label="About Me" />
+//                     </Tabs>
+//                 </nav>
+//             </div>
+//         )
+//     }
+// }
--- src/components/page.js
+++ src/components/page.js
@@ -5,16 +5,6 @@ import green from "material-ui/colors/green";
 
 import NavBar from "./nav-bar";
 
-const customTheme = createMuiTheme({
-    palette: {
-      primary: purple,
-      secondary: green,
-    },
-    status: {
-      danger: 'orange',
-    },
-  });
-
 export default class Page extends React.Component {
     render() {
         return (
