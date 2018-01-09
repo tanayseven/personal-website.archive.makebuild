0b9880b38ed2114219d6efe2459a0a063e436168
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-01-09 23:19:02 +0530

    eslint fixes

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# React-Static - Basic Example
+Tanay's personal website
+========================
 
-This example is the most basic version of react-static available. It includes:
-- Babel
-- CSS imports
-- Image imports
-- File imports
-- Automatic Routing
+![build-status](https://travis-ci.org/tanayseven/personal_website.svg?branch=master)
+[![Website](https://img.shields.io/website-up-down-green-red/https/tanayseven.com.svg?label=hosted_on_server)](https://tanayseven.com)
+[![License](https://img.shields.io/github/license/tanayseven/personal_website.svg)](LICENSE.txt)
+[![Code Climate](https://img.shields.io/codeclimate/coverage/github/tanayseven/personal_website.svg)](https://codeclimate.com/github/tanayseven/personal_website)
 
-To get started, run `react-static create` and use the `basic` template.
+This is my personal website which I use to post my own content (usually blogs)
+
+Instructions:
+-------------
+Make sure you have `sshpass` and `rsync` installed
+```bash
+npm i eslint@laster --save-dev
+npm i eslint-plugin-import@latest --save-dev
+npm i eslint-plugin-jsx-a11y@latest --save-dev
+npm i eslint-plugin-class-property@latest --save-dev
+npm i eslint-config-react-tools@latest --save-dev
+npm i eslint-plugin-react@latest --save-dev
+npm i eslint-config-react-tools@latest --save-dev
+```
+
+License
+-------
+The MIT License (MIT)
--- static.config.js
+++ static.config.js
@@ -2,55 +2,55 @@ const blogList = [
   '2016-03-27-hello-world',
   '2016-07-14-finally-an-android-app',
   '2016-07-18-at-nelkinda-coderetreat',
-].sort();
+].sort()
 
-function moduleDataFor(moduleName) {
-  const componentPath = './src/posts/'+moduleName;
-  let data = require(componentPath).data
+function moduleDataFor (moduleName) {
+  const componentPath = `./src/posts/${moduleName}`
+  /* eslint-disable */
+  const data = require(componentPath).data
+  /* eslint-enable */
   data.componentPath = componentPath
-  return data;
+  return data
 }
 
-var posts = [];
-blogList.forEach(function(element) {
-  posts.push(moduleDataFor(element));
-}, this);
+const posts = []
+blogList.forEach(element => {
+  posts.push(moduleDataFor(element))
+}, this)
 export default {
   getSiteProps: () => ({
     title: 'React Static',
   }),
-  getRoutes: async () => {
-    return [
-      {
-        path: '/',
-        component: 'src/containers/Home',
-      },
-      {
-        path: '/about',
-        component: 'src/containers/About',
-      },
-      {
-        path: '/resume',
-        component: 'src/containers/Resume',
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
+  getRoutes: async () => [
+    {
+      path: '/',
+      component: 'src/containers/Home',
+    },
+    {
+      path: '/about',
+      component: 'src/containers/About',
+    },
+    {
+      path: '/resume',
+      component: 'src/containers/Resume',
+    },
+    {
+      path: '/blog',
+      component: 'src/containers/Blog',
+      getProps: () => ({
+        posts,
+      }),
+      children: posts.map(post => ({
+        path: `/post/${post.id}`,
+        component: post.componentPath,
         getProps: () => ({
-          posts,
+          post,
         }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: post.componentPath,
-          getProps: () => ({
-            post,
-          }),
-        })),
-      },
-      {
-        is404: true,
-        component: 'src/containers/404',
-      },
-    ]
-  },
+      })),
+    },
+    {
+      is404: true,
+      component: 'src/containers/404',
+    },
+  ],
 }
