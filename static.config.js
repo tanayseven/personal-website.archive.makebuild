5743121f7a85b27f4a21a7f31bf9ab1540a225a6
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-01-10 00:09:48 +0530

    Fix SICP challenge post to appropriate JSX, add it to static.con.js

--- static.config.js
+++ static.config.js
@@ -2,6 +2,7 @@ const blogList = [
   '2016-03-27-hello-world',
   '2016-07-14-finally-an-android-app',
   '2016-07-18-at-nelkinda-coderetreat',
+  '2018-01-03-sicp-challenge',
 ].sort()
 
 function moduleDataFor (moduleName) {
