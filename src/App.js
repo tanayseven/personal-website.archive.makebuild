437367339725af4a287095f97550635a7184482a
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-01-21 18:37:31 +0530

    Make the nav bar centered

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Router, Link } from 'react-static'
-//
 import Routes from 'react-static-routes'
 
 import './app.css'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing.unit * 3,
+    backgroundColor: theme.palette.background.paper,
+  },
+})
+
+const navBarStyle = {
+  display: 'inline-block',
+  'list-style-type': 'none',
+}
 
 export default () => (
   <Router>
     <div>
       <nav>
-        <span>Tanay PrabhuDesai</span>
-        <Link to="/">Home</Link>
-        <Link to="/resume">Résumé</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/about">About</Link>
+        <span style={navBarStyle}>Tanay PrabhuDesai</span>
+        <Link to="/" style={navBarStyle}>Home</Link>
+        <Link to="/resume" style={navBarStyle}>Résumé</Link>
+        <Link to="/blog" style={navBarStyle}>Blog</Link>
+        <Link to="/about" style={navBarStyle}>About</Link>
       </nav>
       <div className="content">
         <Routes />
