4af1e83bfbe1ba65a931f2ef021386e0836a7b7d
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-01-21 20:38:57 +0530

    Add react props for nav-bar animations

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
 import Tabs, { Tab } from 'material-ui/Tabs'
-
+import { MuiThemeProvider } from 'material-ui/styles'
+import theme from './theme'
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selection: 1,
+    }
+  }
+  handleChange = (event, value) => {
+    this.setState({selection: value})
+  }
   render() {
     return (
-      <Paper>
-        <Tabs
-          centered
-        >
-          <Tab label="Tanay PrabhuDesai" disabled />
-          <Tab to="/" label="Home" />
-          <Tab to="/resume" label="Résumé" />
-          <Tab to="/blog" label="Blog" />
-          <Tab to="/about" label="About" />
-        </Tabs>
-      </Paper>
+      <MuiThemeProvider theme={theme}>
+        <Paper>
+          <Tabs
+            centered
+            value={this.state.selection}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={this.handleChange}
+          >
+            <Tab label="Tanay PrabhuDesai" disabled />
+            <Tab to="/" label="Home" />
+            <Tab to="/resume" label="Résumé" />
+            <Tab to="/blog" label="Blog" />
+            <Tab to="/about" label="About" />
+          </Tabs>
+        </Paper>
+      </MuiThemeProvider>
     )
   }
 }
-export default NavBar;
+export default NavBar
