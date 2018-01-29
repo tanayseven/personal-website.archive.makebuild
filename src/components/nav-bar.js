8f1c4efe1a0fde2f8e8f570c074ac48ab63b8a7a
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-01-30 00:20:59 +0530

    Added link which are high performance

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,18 +3,22 @@ import Tabs, {Tab} from 'material-ui/Tabs'
 import {MuiThemeProvider} from 'material-ui/styles'
 import Card from 'material-ui/Card'
 import theme from './theme'
+import {Link} from "react-static";
 
 class NavBar extends React.Component {
   constructor (props) {
     super(props)
-    this.pageList = ['', '/', '/resume', '/blog', '/about'],
+    this.home =
+    this.resume = <Link to='/resume'/>
+    this.blog = <Link to='/blog'/>
+    this.about = <Link to='/about'/>
+    this.pageList = ['', '/', '/resume', '/blog', '/about']
     this.state = {
       selection: this.pageList.indexOf('/'+window.location.pathname.split('/')[1]),
     }
   }
   handleChange = (event, value) => {
     this.setState({selection: value})
-    setTimeout(() => window.open(this.pageList[value], '_self'), 300)
   }
   render () {
     return (
@@ -34,10 +38,10 @@ class NavBar extends React.Component {
               onChange={this.handleChange}
             >
               <Tab label="Tanay PrabhuDesai" disabled />
-              <Tab to="/" label="Home" />
-              <Tab to="/resume" label="Résumé" />
-              <Tab to="/blog" label="Blog" />
-              <Tab to="/about" label="About" />
+              <Tab to="/" label={<Link to='/'>Home</Link>} onClick={this.home.onClick} />
+              <Tab to="/resume" label={<Link to='/resume'>Résumé</Link>} onClick={this.resume.onClick} />
+              <Tab to="/blog" label={<Link to='/blog'>Blog</Link>} onClick={this.blog.onClick} />
+              <Tab to="/about" label={<Link to='/about'>About</Link>} onClick={this.about.onClick} />
             </Tabs>
           </Card>
         </div>
