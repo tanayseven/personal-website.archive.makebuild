02899850ca2fbd92d4e9938cfe38cc594299d6de
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-02-20 22:07:11 +0530

    Fix the nav bar and get it working

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,41 +1,37 @@
 import React from 'react'
 import {Link} from "react-static";
 
-class NavBarButton extends React.Component {
-  constructor(props) {
-    super(props)
-    const path = typeof window !== 'undefined' ? window.location.pathname : ''
-    const buttonClasses = '/' + path.split('/')[1] === this.props.link ? 'active disabled' : ''
-    this.state = {
-      buttonClasses: buttonClasses
-    }
-  }
-  render() {
-    return (
-      <li className={this.state.buttonClasses}><Link to={this.props.link}>{this.props.label}</Link></li>
-    )
-  }
-}
-
 class NavBar extends React.Component {
   constructor (props) {
     super(props)
   }
+
+  compareCurrentLocationWith(link) {
+    return link === '/' + window.location.pathname.split('/')[1] ? 'active disabled' : ''
+  }
+
   render () {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="brand-logo left disabled">Tanay PrabhuDesai</a>
           <ul className="right">
-            <NavBarButton link='/' label='Home' />
-            <NavBarButton link='/resume' label='Résumé' />
-            <NavBarButton link='/blog' label='Blog' />
-            <NavBarButton link='/about' label='About Me' />
+            <li className={this.compareCurrentLocationWith('/')}>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className={this.compareCurrentLocationWith('/resume')}>
+              <Link to='/resume'>Résumé</Link>
+            </li>
+            <li className={this.compareCurrentLocationWith('/blog')}>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li className={this.compareCurrentLocationWith('/about')}>
+              <Link to='/about'>About Me</Link>
+            </li>
           </ul>
         </div>
       </nav>
     )
-    // this.state.buttonClicked = false;
   }
 }
 export default NavBar
