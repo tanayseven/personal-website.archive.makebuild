f3d0a2b7c05b265e75fcb8d308adbff427e9509f
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-08-15 19:47:59 +0530

    Get rid of jQuery

--- res/main.js
+++ res/main.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
-  $('.collapsible').collapsible({
-    accordion: true,
-  });
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-})
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  const sideNavOptions = {};
+  const sideNavInstances = M.Sidenav.init(elems, sideNavOptions);
+  var elems = document.querySelectorAll('.collapsible');
+  const collapsibleOptions = {accordion: true};
+  const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
+});
+
