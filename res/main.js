c6075ba9a06829e8b25e1b747969ad90058f5cf4
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-08-25 16:37:34 +0530

    Remove the form from the index page and move it outiside

--- res/main.js
+++ res/main.js
@@ -5,8 +5,4 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   const collapsibleOptions = {accordion: true};
   const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
-  const contact_form_request=new XMLHttpRequest();
-  contact_form_request.open('get','/contact_form.txt');
-  contact_form_request.send();
-  contact_form_request.onload=function(){document.getElementById('contact').innerHTML=contact_form_request.responseText}
 });
