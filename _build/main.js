11b3c42417f33bf028e5706a2f75a26e98b6d395
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-08-25 17:12:32 +0530

    Add obfuscated email address

--- _build/main.js
+++ _build/main.js
@@ -1,3 +1,9 @@
+const setContactEmailAddress = function () {
+  const emailElement = document.querySelector('#email-link')
+  const obfuscatedEmailAddress = 'abcdtabcdanabcdayabcdseabcdveabcdn@gmail.com'
+  emailElement.innerHTML = obfuscatedEmailAddress.replace(/abcd/g, '')
+  emailElement.setAttribute('href', 'mailto:'+obfuscatedEmailAddress.replace(/abcd/g, ''))
+}
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   const sideNavOptions = {};
@@ -5,4 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   const collapsibleOptions = {accordion: true};
   const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
+  setContactEmailAddress();
 });
