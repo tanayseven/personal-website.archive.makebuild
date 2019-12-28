05824ea45681d8324fbb242bade47e7c7e60154b
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-12-28 16:21:56 +0000

    Exit if build fails

--- shell-scripts/build.sh
+++ shell-scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 pipenv run make clean
-pipenv run make build -j
+pipenv run make build -j || exit 1
 aws s3 sync _build s3://tanayseven-personal-website-build/$TRAVIS_BUILD_NUMBER/_build
