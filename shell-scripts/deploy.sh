0954627cf15c42e9c227f5f1037e9383ab8ba801
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-10-12 23:43:56 +0100

    Remove travis-ci related files

--- shell-scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git clone https://${GH_TOKEN}@github.com/tanayseven/tanayseven.github.io.git
-aws s3 sync s3://tanayseven-personal-website-build/$TRAVIS_BUILD_NUMBER/_build _build
-pipenv run make deploy
--- shell-scripts/verify.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-aws s3 sync s3://tanayseven-personal-website-build/$TRAVIS_BUILD_NUMBER/_build _build
-pipenv run make verify
